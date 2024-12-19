42dca96e883e06881cc8b3f4a71b310c5616fd33
正北飞 <1214569257@qq.com>
2024-12-19 21:50:32 +0800

    远程编译

---
+++ MBCoder/MBCoder/Group2/vc3/IBGroup2Controller34.h
@@ -0,0 +1,17 @@
+//
+//  IBGroup2Controller34.h
+//  MBCoder
+//
+//  Created by 叶修 on 2024/12/13.
+//  Copyright © 2024 inke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IBGroup2Controller34 : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MBCoder/MBCoder/Group2/vc3/IBGroup2Controller34.m
@@ -0,0 +1,22 @@
+//
+//  IBGroup2Controller34.m
+//  MBCoder
+//
+//  Created by 叶修 on 2024/12/13.
+//  Copyright © 2024 inke. All rights reserved.
+//
+
+#import "IBGroup2Controller34.h"
+
+@interface IBGroup2Controller34 ()
+
+@end
+
+@implementation IBGroup2Controller34
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+@end

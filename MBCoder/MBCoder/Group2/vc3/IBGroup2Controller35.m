827d98132c680feee44ca9c23776a3a023f856a7
正北飞 <1214569257@qq.com>
2024-12-20 14:51:15 +0800

    hook

--- MBCoder/MBCoder/Group2/vc3/IBGroup2Controller35.m
+++ MBCoder/MBCoder/Group2/vc3/IBGroup2Controller35.m
@@ -21,29 +21,19 @@
 @end
 
 /**
- 一、JSPatch
- JSPatch 实现原理详解：https://cloud.tencent.com/developer/article/1030743
- 源码：https://github.com/bang590/JSPatch
- 源码解析：https://zhang759740844.github.io/2019/08/01/jspatch/
- JSPatch源码解读: https://juejin.cn/post/6844903521322532872
+ Dobby源码
+ https://github.com/jmpews/Dobby
+ iOS逆向实验室｜如何Hook静态语言？- Dobby
+ https://juejin.cn/post/7033349361174233119
+ 13、fishhook原理&Dobby
+ https://blog.csdn.net/SharkToping/article/details/130249309
+ 16-Hook原理（三）InlineHook
+ https://www.jianshu.com/p/bac77377b5cb
  
- 二、OCRunner
- https://github.com/SilverFruity/OCRunner
- https://mp.weixin.qq.com/s?__biz=MzI2NTAxMzg2MA==&mid=2247488962&idx=1&sn=cdb1cd8a8cd788b15fbc0dbc0ecfccf1&scene=21#wechat_redirect
- 
- 三、libffi/lex/yacc
- https://github.com/libffi/libffi
- 
- 四、QQ浏览器HD iOS 动态化/热修复方案QBDF
- https://github.com/ventureli/QBDF
- 
- 五、基于 Aspects 的热修
- 
- 六、MangoFix
- https://www.jianshu.com/p/a6511c687eda
- https://github.com/yanshuimu/MangoFixUtil
- 
- 七、DynamicCocoa
- https://juejin.cn/post/6844903457699135495
- 
+ 一文读懂fishhook原理
+ https://juejin.cn/post/6857699952563978247
+ iOS优秀第三方源码解析（一、深入理解fishhook源码）
+ https://juejin.cn/post/6844904094734237704
+ fishhook 源码
+ https://github.com/facebook/fishhook
  */

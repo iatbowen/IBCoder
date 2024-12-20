59ba16d880d6ab6cdee29e7fa2625c6864872676
正北飞 <1214569257@qq.com>
2024-12-20 11:37:50 +0800

    热修

---
+++ MBCoder/MBCoder/Group2/vc3/IBGroup2Controller35.h
@@ -0,0 +1,17 @@
+//
+//  IBGroup2Controller35.h
+//  MBCoder
+//
+//  Created by 叶修 on 2024/12/20.
+//  Copyright © 2024 inke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IBGroup2Controller35 : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MBCoder/MBCoder/Group2/vc3/IBGroup2Controller35.m
@@ -0,0 +1,49 @@
+//
+//  IBGroup2Controller35.m
+//  MBCoder
+//
+//  Created by 叶修 on 2024/12/20.
+//  Copyright © 2024 inke. All rights reserved.
+//
+
+#import "IBGroup2Controller35.h"
+
+@interface IBGroup2Controller35 ()
+
+@end
+
+@implementation IBGroup2Controller35
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
+
+/**
+ 一、JSPatch
+ JSPatch 实现原理详解：https://cloud.tencent.com/developer/article/1030743
+ 源码：https://github.com/bang590/JSPatch
+ 源码解析：https://zhang759740844.github.io/2019/08/01/jspatch/
+ JSPatch源码解读: https://juejin.cn/post/6844903521322532872
+ 
+ 二、OCRunner
+ https://github.com/SilverFruity/OCRunner
+ https://mp.weixin.qq.com/s?__biz=MzI2NTAxMzg2MA==&mid=2247488962&idx=1&sn=cdb1cd8a8cd788b15fbc0dbc0ecfccf1&scene=21#wechat_redirect
+ 
+ 三、libffi/lex/yacc
+ https://github.com/libffi/libffi
+ 
+ 四、QQ浏览器HD iOS 动态化/热修复方案QBDF
+ https://github.com/ventureli/QBDF
+ 
+ 五、基于 Aspects 的热修
+ 
+ 六、MangoFix
+ https://www.jianshu.com/p/a6511c687eda
+ https://github.com/yanshuimu/MangoFixUtil
+ 
+ 七、DynamicCocoa
+ https://juejin.cn/post/6844903457699135495
+ 
+ */

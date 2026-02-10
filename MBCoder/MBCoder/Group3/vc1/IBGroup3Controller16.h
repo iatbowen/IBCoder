2a0478a50d2fc26a970033659c8dbf4d9a2a5a47
正北飞 <1214569257@qq.com>
2026-02-10 16:27:11 +0800

    补全

---
+++ MBCoder/MBCoder/Group3/vc1/IBGroup3Controller16.h
@@ -0,0 +1,17 @@
+//
+//  IBGroup3Controller16.h
+//  MBCoder
+//
+//  Created by 叶修 on 2026/2/9.
+//  Copyright © 2026 inke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IBGroup3Controller16 : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MBCoder/MBCoder/Group3/vc1/IBGroup3Controller16.m
@@ -0,0 +1,29 @@
+//
+//  IBGroup3Controller16.m
+//  MBCoder
+//
+//  Created by 叶修 on 2026/2/9.
+//  Copyright © 2026 inke. All rights reserved.
+//
+
+#import "IBGroup3Controller16.h"
+
+@interface IBGroup3Controller16 ()
+
+@end
+
+@implementation IBGroup3Controller16
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
+
+/*
+ OpenSpec 与 SpecKit：AI 时代的规范驱动开发新实践
+ https://jimmysong.io/zh/book/ai-handbook/sdd/openspec/
+ 
+ 
+ 
+ */
